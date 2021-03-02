6b5c228b16347ad84a3d18aacfde70d85fec9912
zhuny <zhuny@kaist.ac.kr>
2021-03-03 01:05:21 +0900

    initialize

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='algebra',
+    version='0.0.1',
+    description='Symbolic Algebra Implementation',
+    url='git@github.com:zhuny/algebra',
+    author='Jihun Yang',
+    author_email='zhuny936772@gmail.com',
+    license='unlicense',
+    packages=['algebra'],
+    zip_safe=False
+)
