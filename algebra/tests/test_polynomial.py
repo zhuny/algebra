9e7ee78393c378eba00264b06320000b2c23d881
zhuny <zhuny@kaist.ac.kr>
2021-03-25 00:58:14 +0900

    coverage for polynomial

--- algebra/tests/test_polynomial.py
+++ algebra/tests/test_polynomial.py
@@ -23,3 +23,21 @@ class TestPolynomial(unittest.TestCase):
         p2 = p1 * p1
         p3 = Polynomial({i: min(i+1, 199-i) for i in range(199)})
         self.assertEqual(p2, p3)
+
+    def test_error(self):
+        p = Polynomial({})
+
+        with self.subTest("__mul__ type error"):
+            with self.assertRaises(ValueError):
+                p = p * ()
+                print(p)
+
+        with self.subTest("__truediv__ type error"):
+            with self.assertRaises(ValueError):
+                p = p / ()
+                print(p)
+
+    def test_diff(self):
+        p = Polynomial({1: 1, 2: 1, 3: 1, 4:1})
+        self.assertEqual(p.diff(), Polynomial({0: 1, 1: 2, 2: 3, 3: 4}))
+        print(p.diff().to_wolfram_alpha())
