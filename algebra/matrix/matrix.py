a37c1f6d7a6cf9d4f6ceaecbe10680c55b3e93a6
zhuny <zhuny@kaist.ac.kr>
2021-12-02 01:03:54 +0900

    add all row

--- algebra/matrix/matrix.py
+++ algebra/matrix/matrix.py
@@ -31,13 +31,13 @@ class Matrix:
         else:
             raise ValueError("Index out of matrix size")
 
-    def _div(self, row, value):
+    def _div_row(self, row, value):
         if value == 1:
             return
         for col in range(self.col_size):
             self[row, col] /= value
 
-    def _add(self, row_s, row_t, value):
+    def _add_row(self, row_s, row_t, value):
         for col in range(self.col_size):
             self[row_t, col] += self[row_s, col] * value
 
@@ -46,7 +46,7 @@ class Matrix:
             if self[row2, col]:
                 return row2
 
-    def _swap(self, row1, row2):
+    def _swap_row(self, row1, row2):
         for col in range(self.col_size):
             self[row1, col], self[row2, col] = self[row2, col], self[row1, col]
 
@@ -60,7 +60,7 @@ class Matrix:
                     continue
                 else:
                     if row != row2:
-                        self._swap(row, row2)
+                        self._swap_row(row, row2)
                         associate[row], associate[row2] = (
                             associate[row2], associate[row]
                         )
@@ -70,13 +70,13 @@ class Matrix:
                 break
 
             associate[row] /= self[row, col]
-            self._div(row, self[row, col])
+            self._div_row(row, self[row, col])
 
             for row2 in range(self.row_size):
                 if row == row2:
                     continue
                 associate[row2] -= associate[row] * self[row2, col]
-                self._add(row, row2, -self[row2, col])
+                self._add_row(row, row2, -self[row2, col])
 
     def is_zero_row(self, row):
         for col in range(self.col_size):
