b7a04e825771a7969371214c2310ba690f9fb87c
zhuny <zhuny936772@gmail.com>
2023-03-27 01:24:44 +0900

    more test set

--- algebra/tests/test_group_center.py
+++ algebra/tests/test_group_center.py
@@ -5,26 +5,43 @@ from algebra.group.abstract.permutation import PermutationGroupRep
 
 class TestGroupCenter(unittest.TestCase):
     def test_symmetric_group(self):
-        perm = PermutationGroupRep(7)
+        """
+        S_n is trivial for n >= 3
+        """
+        factorial = 2
+        for n in range(3, 8):
+            factorial *= n
+
+            with self.subTest(f"Center of S_{n} is trivial"):
+                perm = PermutationGroupRep(n)
                 ol = list(perm.object_list())
 
-        group = perm.group(perm.element(ol), perm.element(ol[2:]))
+                group = perm.group(perm.element(ol), perm.element(ol[:2]))
+
+                self.assertEqual(group.order(), factorial)
 
                 symm_center = group.center()
                 self.assertTrue(symm_center.is_trivial())
 
     def test_abelian_group(self):
-        perm = PermutationGroupRep(8)
-        ol = list(perm.object_list())
+        """
+        Center of abelian group is itself
+        """
+        for x in range(2, 5):
+            for y in range(2, 5):
+                perm = PermutationGroupRep(x + y)
+                ol = list(perm.object_list())
 
-        group = perm.group(
-            perm.element(ol[:5]),
-            perm.element(ol[5:])
-        )
-        self.assertEqual(group.order(), 15)
+                group = perm.group(
+                    perm.element(ol[:x]),
+                    perm.element(ol[x:])
+                )
 
-        center = group.center()
-        self.assertEqual(center.order(), 15)
+                # TODO: Check this group is abelian
+                self.assertEqual(group.order(), x * y)
+
+                center = group.center()
+                self.assertEqual(center.order(), x * y)
 
     def test_dihedral_even(self):
         """
