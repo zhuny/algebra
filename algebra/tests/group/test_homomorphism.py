e741418247882605dffce37a4892915bfefdb0f9
zhuny <zhuny936772@gmail.com>
2023-09-26 01:07:51 +0900

    symmetric and alternative

--- algebra/tests/group/test_homomorphism.py
+++ algebra/tests/group/test_homomorphism.py
@@ -1,6 +1,7 @@
 import unittest
 
-from algebra.group.abstract.shortcut import dihedral_group, symmetric_group
+from algebra.group.abstract.shortcut import dihedral_group, symmetric_group, \
+    cyclic_group, alternative_group
 from algebra.group.homomorphism import GroupHomomorphism
 
 
@@ -29,3 +30,27 @@ class TestHomomorphism(unittest.TestCase):
         hom_image = hom.image()
         self.assertEqual(hom_image.order(), 1)  # trivial group
         self.assertEqual(hom.kernel().order(), domain.order())
+
+    def test_symmetric_with_al(self):
+        # Domain Setting
+        n = 8
+        domain = alternative_group(n)
+        ol = list(domain.represent.object_list())
+        domain.generator.append(
+            domain.represent.element(ol[:2])
+        )
+        self.assertEqual(domain.order(), 40_320)
+
+        # Codomain Setting
+        codomain = cyclic_group(2)
+        one = codomain.generator[0]
+        zero = codomain.represent.identity
+
+        mapping = {
+            g: zero if g.order() == 3 else one
+            for g in domain.generator
+        }
+        hom = GroupHomomorphism(domain, codomain, mapping)
+
+        self.assertEqual(hom.image().order(), 2)
+        self.assertEqual(hom.kernel().order(), 20_160)
