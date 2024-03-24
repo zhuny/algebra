c3f94f2f4257b9f9edd5ab5ed429543a95826fb2
zhuny <zhuny936772@gmail.com>
2024-03-25 02:28:45 +0900

    update version

--- gen-badge.bat
+++ gen-badge.bat
@@ -1,2 +1,2 @@
-python -m pybadges --left-text=python --right-text=3.8 > badge\python.svg
+python -m pybadges --left-text=python --right-text=3.11 > badge\python.svg
 python -m pybadges --left-text=algebra --right-text=0.0 > badge\algebra.svg
